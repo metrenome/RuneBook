477fc95fd2f1388a30092c041a058ab496dd71ac
Jon Lee <metrenome@gmail.com>
2020-04-29 13:25:09 -0400

    Set up Electron and MaterializeCSS
    Added Electron and set up MaterializeCSS.

---
+++ build/precache-manifest.96c5a776f4b6a07fc53a216aae289058.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ff75e551263dbbb31767ffeac0380577",
+    "url": "./index.html"
+  },
+  {
+    "revision": "58d8d9f32f1825997d51",
+    "url": "./static/css/main.7df995af.chunk.css"
+  },
+  {
+    "revision": "fb7fbd7c1236e4954b2f",
+    "url": "./static/js/2.1049eebd.chunk.js"
+  },
+  {
+    "revision": "58d8d9f32f1825997d51",
+    "url": "./static/js/main.29363139.chunk.js"
+  },
+  {
+    "revision": "209a1b60eb7b396a8c16",
+    "url": "./static/js/runtime-main.04eee1eb.js"
+  },
+  {
+    "revision": "81f8a98460ee2f267c6cc22e163f5b51",
+    "url": "./static/media/logo.81f8a984.svg"
+  }
+]);
